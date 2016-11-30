45dcdbf40dfe9ce8aedfcae095c3b297298043ab
Tobias Schwarz <toschdev@gmail.com>
2016-11-30 22:49:09 +0100

    udate

--- index.js
+++ index.js
@@ -5,27 +5,45 @@ var adc = new McpAdc.Mcp3208();
 var channel = 0;
 var voltOffset = 1650;
 
+var totalRaw = 0;
+var totalVolt = 0;
+var totalPerc = 0;
 
-
-setInterval(function() {
+for(var x=0; x<100;x++) {
 
     adc.readRawValue(channel, function(value) {
-        console.log("Raw value:\t" + value);
+        totalRaw = totalWar + value;
+
     });
 
     adc.readVoltage(channel, function(voltage) {
 
-        var mV = voltage * 1000;
-        console.log("Voltage:\t" + voltage);
-
-        var ampere = (mV - voltOffset) / 100;
-        console.log("Ampere:\t" + ampere);
+        totalVolt = totalVolt + voltage;
 
     });
 
     adc.readNormalizedValue(channel, function(normValue) {
-        console.log("Percents:\t" + (normValue * 100));
+        totalPerc = totalPerc + normValue;
     });
+
+}
+
+var avgVolt = totalVolt / 100;
+var avgRaw = totalRaw / 100;
+var avgPerc = totalPerc / 100;
+
+setInterval(function() {
+
+        console.log("Raw value:\t" + avgRaw);
+
+        var mV = avgVolt * 1000;
+        console.log("avgVolt:\t" + voltage);
+
+        var ampere = (mV - voltOffset) / 100;
+        console.log("Ampere:\t" + ampere);
+
+
+        console.log("Percents:\t" + (avgPerc * 100));
 
 
 }, 1000);
