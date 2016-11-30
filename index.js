a234b115c641a558313097957a8f5c3b284678a8
Tobias Schwarz <toschdev@gmail.com>
2016-11-30 23:15:48 +0100

    measurement updates

--- index.js
+++ index.js
@@ -12,41 +12,31 @@ setInterval(function() {
     var totalVolt = 0;
     var totalPerc = 0;
 
-    for(var x=0; x<1000;x++) {
+
+
+    for(var x=0; x<10000;x++) {
 
 
             adc.readRawValue(channel, function(value) {
                 totalRaw = totalRaw + value;
             });
 
-            adc.readVoltage(channel, function(voltage) {
-
-                totalVolt = totalVolt + voltage;
-
-            });
-
-            adc.readNormalizedValue(channel, function(normValue) {
-                totalPerc = totalPerc + normValue;
-            });
-
-
     }
 
-    var avgVolt = totalVolt / 1000;
-    var avgRaw = totalRaw / 1000;
-    var avgPerc = totalPerc / 1000;
-
+    var avgRaw = totalRaw / 10000;
 
     console.log("Raw value:\t" + avgRaw);
 
+    var avgVolt = ((avgRaw * 3.3) / 4095);
+
     console.log("avgVolt:\t" + avgVolt);
 
-    var mV = avgVolt * 1000;
-    var ampere = (mV - voltOffset) / 100;
+
+    var ampere = (avgVolt / 10) - 0.1;
     console.log("Ampere:\t" + ampere);
 
 
-    console.log("Percents:\t" + (avgPerc * 100));
+    //console.log("Percents:\t" + (avgPerc * 100));
 
 
 }, 3000);
