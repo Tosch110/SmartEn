02c296670c36397ea5014aa26cec688cc0717650
Tobias Schwarz <toschdev@gmail.com>
2016-11-30 21:30:27 +0100

    add Ampere

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ var McpAdc = require('mcp-adc');
 var adc = new McpAdc.Mcp3208();
 
 var channel = 0;
+var voltOffset = 1690;
 
 adc.readRawValue(channel, function(value) {
         console.log("Raw value:\t" + value);
 });
 
 adc.readVoltage(channel, function(voltage) {
+        var mV = voltage * 1000;
         console.log("Voltage:\t" + voltage);
+
+        var difference = mV - voltOffset;
+        console.log("Ampere:\t" + difference);
+
+
+
 });
 
 adc.readNormalizedValue(channel, function(normValue) {
