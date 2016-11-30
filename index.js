096c575cefc94ca5a9cc2661c0f787afa3d094e7
Tobias Schwarz <toschdev@gmail.com>
2016-11-30 22:50:48 +0100

    update averages

--- index.js
+++ index.js
@@ -5,46 +5,47 @@ var adc = new McpAdc.Mcp3208();
 var channel = 0;
 var voltOffset = 1650;
 
-var totalRaw = 0;
-var totalVolt = 0;
-var totalPerc = 0;
-
-for(var x=0; x<100;x++) {
-
-    adc.readRawValue(channel, function(value) {
-        totalRaw = totalWar + value;
-
-    });
-
-    adc.readVoltage(channel, function(voltage) {
-
-        totalVolt = totalVolt + voltage;
-
-    });
-
-    adc.readNormalizedValue(channel, function(normValue) {
-        totalPerc = totalPerc + normValue;
-    });
-
-}
-
-var avgVolt = totalVolt / 100;
-var avgRaw = totalRaw / 100;
-var avgPerc = totalPerc / 100;
-
 setInterval(function() {
 
-        console.log("Raw value:\t" + avgRaw);
 
-        var mV = avgVolt * 1000;
-        console.log("avgVolt:\t" + voltage);
+    var totalRaw = 0;
+    var totalVolt = 0;
+    var totalPerc = 0;
 
-        var ampere = (mV - voltOffset) / 100;
-        console.log("Ampere:\t" + ampere);
+    for(var x=0; x<100;x++) {
+
+        adc.readRawValue(channel, function(value) {
+            totalRaw = totalRaw + value;
+        });
+
+        adc.readVoltage(channel, function(voltage) {
+
+            totalVolt = totalVolt + voltage;
+
+        });
+
+        adc.readNormalizedValue(channel, function(normValue) {
+            totalPerc = totalPerc + normValue;
+        });
+
+    }
+
+    var avgVolt = totalVolt / 100;
+    var avgRaw = totalRaw / 100;
+    var avgPerc = totalPerc / 100;
 
 
-        console.log("Percents:\t" + (avgPerc * 100));
+    console.log("Raw value:\t" + avgRaw);
+
+    var mV = avgVolt * 1000;
+    console.log("avgVolt:\t" + voltage);
+
+    var ampere = (mV - voltOffset) / 100;
+    console.log("Ampere:\t" + ampere);
 
 
-}, 1000);
+    console.log("Percents:\t" + (avgPerc * 100));
+
+
+}, 2000);
 
