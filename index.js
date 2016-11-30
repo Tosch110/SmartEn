c073d6c6cb3104138a5a28dfc64efeb3a87e05be
Tobias Schwarz <toschdev@gmail.com>
2016-11-30 21:41:19 +0100

    offset

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ var McpAdc = require('mcp-adc');
 var adc = new McpAdc.Mcp3208();
 
 var channel = 0;
-var voltOffset = 1690;
+var voltOffset = 1650;
 
 adc.readRawValue(channel, function(value) {
-        console.log("Raw value:\t" + value);
+    console.log("Raw value:\t" + value);
 });
 
 adc.readVoltage(channel, function(voltage) {
-        var mV = voltage * 1000;
-        console.log("Voltage:\t" + voltage);
+    var mV = voltage * 1000;
+    console.log("Voltage:\t" + voltage);
 
-        var ampere = (mV - voltOffset) / 100;
-        console.log("Ampere:\t" + ampere);
+    var ampere = (mV - voltOffset) / 100;
+    console.log("Ampere:\t" + ampere);
 
 
 
 });
 
 adc.readNormalizedValue(channel, function(normValue) {
-        console.log("Percents:\t" + (normValue * 100));
+    console.log("Percents:\t" + (normValue * 100));
 });
