50517571da5ee21034f0c38db356af53b5d417be
Tobias Schwarz <toschdev@gmail.com>
2016-11-30 22:39:09 +0100

    interval

--- index.js
+++ index.js
@@ -5,20 +5,28 @@ var adc = new McpAdc.Mcp3208();
 var channel = 0;
 var voltOffset = 1650;
 
-adc.readRawValue(channel, function(value) {
-    console.log("Raw value:\t" + value);
-});
 
-adc.readVoltage(channel, function(voltage) {
 
-    var mV = voltage * 1000;
-    console.log("Voltage:\t" + voltage);
+setInterval(function() {
 
-    var ampere = (mV - voltOffset) / 100;
-    console.log("Ampere:\t" + ampere);
+    adc.readRawValue(channel, function(value) {
+        console.log("Raw value:\t" + value);
+    });
 
-});
+    adc.readVoltage(channel, function(voltage) {
 
-adc.readNormalizedValue(channel, function(normValue) {
-    console.log("Percents:\t" + (normValue * 100));
-});
+        var mV = voltage * 1000;
+        console.log("Voltage:\t" + voltage);
+
+        var ampere = (mV - voltOffset) / 100;
+        console.log("Ampere:\t" + ampere);
+
+    });
+
+    adc.readNormalizedValue(channel, function(normValue) {
+        console.log("Percents:\t" + (normValue * 100));
+    });
+
+
+}, 1000);
+
