be16223d3b330d2f81c67d4220fb146c504fe67f
Tobias Schwarz <toschdev@gmail.com>
2016-11-30 20:08:27 +0100

    add files

---
+++ index.js
@@ -0,0 +1,17 @@
+var McpAdc = require('mcp-adc');
+
+var adc = new McpAdc.Mcp3208();
+
+var channel = 1;
+
+adc.readRawValue(channel, function(value) {
+        console.log("Raw value:\t" + value);
+});
+
+adc.readVoltage(channel, function(voltage) {
+        console.log("Voltage:\t" + voltage);
+});
+
+adc.readNormalizedValue(channel, function(normValue) {
+        console.log("Percents:\t" + (normValue * 100));
+});
